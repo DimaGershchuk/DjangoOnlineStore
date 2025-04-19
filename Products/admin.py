79f7c11b2d103a8db6db1fddbc3c0ed58668036f
DimaGershchuk <dimagersuk29@gmail.com>
2025-04-19 11:42:43 +0100

    Created and setting up project, added apps Product and Users

---
+++ Products/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import Category, Brand, Product
+
+admin.register(Category)
+admin.register(Brand)
+admin.register(Product)
---
+++ Products/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField()
+    image = models.ImageField(upload_to='images')
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(250, 250)],
+        format='JPEG',
+        options={'quality': 75}
+    )
+
+    average_rating = models.FloatField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, related_name='products')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created'] #descending order
+        indexes = [
+            models.Index(fields=['-created']),
+            models.Index(fields=['category']),
+            models.Index(fields=['brand'])
+        ]
+
+    def __str__(self):
+        return self.name
