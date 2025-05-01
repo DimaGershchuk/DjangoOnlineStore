32e05a1c2775d7667451b53bc8269a708b084b40
DimaGershchuk <dimagersuk29@gmail.com>
2025-05-01 12:30:40 +0100

    Started creating functionality for user profile

--- Users/forms.py
+++ Users/forms.py
@@ -7,7 +7,7 @@ class CustomUserCreationForm(UserCreationForm):
     username = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
     age = forms.IntegerField(label='Age', widget=forms.NumberInput(attrs={'class': 'form-input'}))
-    tel_number = forms.IntegerField(label='Telephone number', widget=forms.NumberInput(attrs={'class': 'form-input'}))
+    tel_number = forms.IntegerField(label='Telephone number', widget=forms.TelInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Password confirmation',
                                 widget=forms.PasswordInput(attrs={'class': 'form-input'}))
@@ -20,9 +20,22 @@ class CustomUserCreationForm(UserCreationForm):
             'username': forms.TextInput(attrs={'class': 'form-input'}),
             'email': forms.EmailInput(attrs={'class': 'form-input'}),
             'age': forms.NumberInput(attrs={'class': 'form-input'}),
-            'tel_number': forms.NumberInput(attrs={'class': 'form-input'}),
+            'tel_number': forms.TelInput(attrs={'class': 'form-input'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-input'})
+        }
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'age', 'tel_number')
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+            'age': forms.NumberInput(attrs={'class': 'form-input'}),
+            'tel_number': forms.TelInput(attrs={'class': 'form-input'})
         }
 
 
@@ -35,7 +48,7 @@ class CustomUserChangeForm(UserChangeForm):
             'username': forms.TextInput(attrs={'class': 'form-input'}),
             'email': forms.EmailInput(attrs={'class': 'form-input'}),
             'age': forms.NumberInput(attrs={'class': 'form-input'}),
-            'tel_number': forms.Select(attrs={'class': 'form-input'}),
+            'tel_number': forms.TelInput(attrs={'class': 'form-input'}),
         }
 
 
