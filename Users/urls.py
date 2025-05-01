ca2603df1a4a270f41c57a97d0aa14f2e0c82b95
DimaGershchuk <dimagersuk29@gmail.com>
2025-05-01 12:30:12 +0100

    Started creating functionality for user profile

--- Users/urls.py
+++ Users/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
-from .views import home_view, RegisterUser, LoginUser, logout_user, CustomUserDetailView, CustomUserListCreateView
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import home_view, RegisterUser, LoginUser, logout_user, CustomUserDetailView, CustomUserListCreateView, ProfileView, ProfileUpdateView
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/', ProfileUpdateView.as_view(), name='profile-edit'),
+    path('profile/password', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+
 
     # Api-endpoints
     path('api/users/', CustomUserListCreateView.as_view(), name='create-user'),
--- Users/views.py
+++ Users/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib.auth import logout
-from .forms import CustomUserCreationForm, LoginUserForm
+from .forms import CustomUserCreationForm, LoginUserForm, ProfileForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from rest_framework import viewsets, permissions, generics
-from rest_framework.authentication import TokenAuthentication
 from .models import CustomUser
 from Products.models import Category
 from .serializers import CustomUserSerializer, CustomUserCreateSerializer
@@ -19,7 +19,7 @@ def home_view(request):
 
 class RegisterUser(CreateView):
     form_class = CustomUserCreationForm
-    template_name = 'registration/register.html'
+    template_name = 'users/register.html'
 
     def get_success_url(self):
         return reverse_lazy('home-page')
@@ -27,7 +27,7 @@ class RegisterUser(CreateView):
 
 class LoginUser(LoginView):
     form_class = LoginUserForm
-    template_name = 'registration/login.html'
+    template_name = 'users/login.html'
 
     def get_success_url(self):
         return reverse_lazy('home-page')
@@ -36,6 +36,25 @@ class LoginUser(LoginView):
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/profile.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['user'] = self.request.user
+        return ctx
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = ProfileForm
+    template_name = 'users/profile-edit.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self):
+        return self.request.user
 
 
 class CustomUserListCreateView(generics.ListCreateAPIView):
